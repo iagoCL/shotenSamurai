3c6cbee9fb211305a1be0e1bc6f94f3127412c12
cruzIago <iagocruzgarcia@gmail.com>
2018-11-26 20:58:42 +0100

    Ladder and gameover
    Ladder and gameover done, needs some testing

--- js/gameOver.js
+++ js/gameOver.js
@@ -1,29 +1,53 @@
 $(document).ready(function () {
 
-    var puntos=sessionStorage.getItem("ultimaPuntuacion");
+    let puntos=sessionStorage.getItem("ultimaPuntuacion");
+    $("#puntos").html(parseInt(puntos));
     if(puntos!=null){
-    var puntuacionesGuardadas = JSON.parse(sessionStorage.getItem("puntuaciones"));
-
-    if(puntuacionesGuardadas!=null){ //Next executions
-        for(var i=0;i<puntuacionesGuardadas.length;i++){
-            if(puntos>=puntuacionesGuardadas[i]){
-                puntuacionesGuardadas.push(puntos);
-                break;
+        var puntuacionesGuardadas = JSON.parse(sessionStorage.getItem("puntuaciones"));
+        if(puntuacionesGuardadas!=null){
+            for(var i=0;i<puntuacionesGuardadas.length;i++){
+                if(puntos>=puntuacionesGuardadas[i].puntos || puntuacionesGuardadas.length<2){
+                    $("#record").show();
+                    break;
+                }else{
+                    $("#record").hide();
+                }
             }
         }
-
-        puntuacionesGuardadas.sort(function(a, b){return b-a});
-        if(puntuacionesGuardadas.length>1){
-            puntuacionesGuardadas.pop();
-        }
-        sessionStorage.setItem("puntuaciones",JSON.stringify(puntuacionesGuardadas));
-    }else{ //For the first execution of gameOver
-        let primerosPuntos=[];
-        primerosPuntos.push(puntos);
-        sessionStorage.setItem("puntuaciones",JSON.stringify(primerosPuntos));
-    }
-    $("#puntos").html(parseInt(puntos));
-    sessionStorage.removeItem("ultimaPuntuacion");
-    }   
+     }
     
 });
+
+function clicked() {
+    let puntos=sessionStorage.getItem("ultimaPuntuacion");
+    let nombre=document.getElementById("newRecord").value;
+    if(nombre=="   " ||nombre=="  "||nombre==" "||nombre==""){
+        nombre="aaa";
+    }
+    if(puntos!=null){
+        var puntuacionesGuardadas = JSON.parse(sessionStorage.getItem("puntuaciones"));
+        if(puntuacionesGuardadas!=null){ //Next executions
+            for(var i=0;i<puntuacionesGuardadas.length;i++){
+                if(puntos>=puntuacionesGuardadas[i].puntos || puntuacionesGuardadas.length<2){
+                    let nPuntos={nombre,puntos};
+                    puntuacionesGuardadas.push(nPuntos);
+                    break;
+                }
+            }
+    
+            puntuacionesGuardadas.sort(function(a, b){return b.puntos-a.puntos});
+            while(puntuacionesGuardadas.length>2){
+                puntuacionesGuardadas.pop();
+            }
+            sessionStorage.setItem("puntuaciones",JSON.stringify(puntuacionesGuardadas));
+        }else{ //For the first execution of gameOver
+            let nPuntos={nombre,puntos};
+            let primerosPuntos=[];
+            primerosPuntos.push(nPuntos);
+            sessionStorage.setItem("puntuaciones",JSON.stringify(primerosPuntos));
+        }
+
+}
+
+sessionStorage.removeItem("ultimaPuntuacion");
+}
