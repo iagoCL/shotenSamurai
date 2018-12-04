d4c5e025ae1d223217e2646933c55cb8a6c65c92
iagoCL <iagocltrabajo@gmail.com>
2018-12-04 16:29:17 +0100

    el logo del juego sale en el menu la primera vez que incias

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+const aspectRatio = 0.817;
+
 $(document).ready(function () {
     $.getJSON("js/languages.json",function(data){
         let langugeData;
@@ -14,4 +16,38 @@ $(document).ready(function () {
         $("#puntuaciones").html(langugeData.puntuaciones);
         $("#config").html(langugeData.config);
     });
+    let firstLoad = JSON.parse(sessionStorage.getItem("firstLoad"));
+    if(firstLoad != null && firstLoad != undefined && !firstLoad)
+    {
+        $("#mainMenu").css({display: "block"});
+    }
+    else
+    {
+        $("#firstGame").css({display: "block"});
+        $(document).click(function(){
+            sessionStorage.setItem("firstLoad",false);
+            $("#mainMenu").css({display: "block"});
+            $("#firstGame").css({display: "none"});
+            $(document).unbind("click");
+        });
+    }
+    resizeMenu();
+    window.addEventListener("resize", resizeMenu, false);
 });
+function resizeMenu() {
+    let imageWidth = 0.895 * window.innerWidth;
+    let imageHeight = 0.895 * window.innerHeight;
+    let imageWidthB = imageHeight / aspectRatio;
+    let imageHeightB = imageWidth * aspectRatio;
+    if (imageWidth > imageWidthB) {
+        imageWidth = Math.floor(imageWidthB);
+        imageHeight = Math.floor(imageHeight);
+    }
+    else {
+        imageHeight = Math.floor(imageHeightB);
+        imageWidth = Math.floor(imageWidth);
+    }
+
+    $("#firstGame").css({left: Math.floor(0.5*(window.innerWidth-imageWidth)), top: Math.floor(0.5*(window.innerHeight-imageHeight)) , width: imageWidth, height: imageHeight});
+    //console.log("resize to w: "+imageWidth + " h: "+imageHeight);
+}
